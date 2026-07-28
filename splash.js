53df3878204045d575bfb72baf702b4cc966be42
globalpaypanel <ajaysingh40cr@gmail.com>
2026-07-28 18:45:54 +0530

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,1 +1,10 @@
+window.onload = function () {
 
+    const audio = new Audio("startup.mp3");
+    audio.play();
+
+    setTimeout(function () {
+        window.location.href = "home.html";
+    }, 3000);
+
+};
