48924530b242a223f0b13c8cfa15c78fd6d3a419
globalpaypanel <ajaysingh40cr@gmail.com>
2026-08-08 13:25:18 +0530

    Update upiwork.js

--- upiwork.js
+++ upiwork.js
@@ -169,6 +169,19 @@ function addTransaction(
     amount
 ){
 
+
+const now = new Date();
+
+const dateTime = now.toLocaleString("en-IN", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true
+});
+
     const list =
         document.getElementById(
             "transactionList"
